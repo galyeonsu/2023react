14da09723db89a2f0f0b1d4679691d036746c89d
gal <www503@naver.com>
2023-11-10 15:39:27 +0900

    component

---
+++ src/App.hsx.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const gal = "안녕하세요."  //model
+  let [listData, setListData] = useState(1)
+  return (
+    <div className="App">
+
+      <div>{listData}</div>
+
+      <button onClick={()=>{setListData(listData+1)}}>button</button>
+    </div>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import './App.css'
 
 function App() {
-  const gal = "안녕하세요."
-  let [listData, setListData] = useState(1)
+  let [data, setData] = useState(1)
+  let [listData, setListData] = useState(['list1', 'list2', 'list3'])
+  let [mView, setMView] = useState(false)
+  let today = new Date();
+  let time = {
+    year: today.getFullYear(),
+    month: today.getMonth() + 1,
+    date: today.getDate()
+  }
   return (
     <div className="App">
-
-      <div>{listData}</div>
-
-      <button onClick={()=>{setListData(listData+1)}}>button</button>
+      <div>{data}</div>
+      <button onClick={(() => { setData(data + 1) })}> 클릭</button>
+      {
+        listData.map(function (item, i) {
+          return (
+            <>
+              <div className='flex'>
+                <div className='list'>{item}</div>
+                <div>{time.year}/{time.month}/{time.date}</div>
+              </div>
+            </>
+          )
+        })
+      }
+      <button onClick={()=>{setMView(!mView)}}>클릭</button>
+   {
+    mView == true ?  <Modal></Modal> : null        
+   }
     </div>
   );
+}
+
+
+function Modal() {
+  return (
+    <>
+      <div className='modal'>
+        <h3>제목</h3>
+        <p>안녕하세요</p>
+      </div>
+    </>
+  )
 }
 
 export default App;
